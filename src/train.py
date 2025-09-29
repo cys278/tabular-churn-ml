11adada0d3e8e70060eed9915e5532d465b2108a
Chowdhury Yasir <cya127@sfu.ca>
2025-09-29 15:49:41 -0700

    Set the transformer

--- src/train.py
+++ src/train.py
@@ -18,3 +18,45 @@ def load_splits():
     y_val = pd.read_csv(f"{PROCESSED_PATH}/y_val.csv")
     y_test = pd.read_csv(f"{PROCESSED_PATH}/y_test.csv")
     return X_train,X_val,X_test,y_train,y_val,y_test
+
+def train_and_eval():
+    #Load data
+
+    X_train,X_val,X_test,y_train,y_val,y_test=load_splits()
+
+    #Identify numeric and categorical columns
+
+    numeric_features=X_train.select_dtypes(include=["int64","float64"]).columns
+    categorical_features=X_train.select_dtypes(include=["object","bool"]).columns
+
+    # Preprocessing
+
+    numeric_transformer=Pipeline(steps=[
+        ("imputer",SimpleImputer(strategy="median")),
+        ("scalar",StandardScaler())
+    ])
+    categorical_transformer=Pipeline(steps=[
+        ("imputer",SimpleImputer(strategy="most_frequent")),
+        ("encoder",OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+    preprocessor=ColumnTransformer(
+        transformers=[
+            ("num",numeric_transformer,numeric_features),
+            ("cat",categorical_transformer,categorical_features)
+        ]
+    )
+
+    #Model
+
+    clf= Pipeline(steps=[
+        ("preprocessor",preprocessor),
+        ("model",LogisticRegression(max_iter=1000))
+    ])
+
+    # Train
+
+    clf.fit(X_train,y_train)
+
+
+
