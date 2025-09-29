95f06492b24e03abc488967cf4d6453527c268a5
Chowdhury Yasir <cya127@sfu.ca>
2025-09-28 23:54:35 -0700

    added download_data.py and downloaded the datasheet in data/raw/

---
+++ src/download_data.py
@@ -0,0 +1,27 @@
+import kagglehub
+import os, shutil
+
+def download_telco_churn():
+    # Download the dataset (from KaggleHub cache)
+    path = kagglehub.dataset_download("blastchar/telco-customer-churn")
+    print("📥 Downloaded dataset to cache:", path)
+
+    # Find the CSV inside
+    csv_name = "WA_Fn-UseC_-Telco-Customer-Churn.csv"
+    src = os.path.join(path, csv_name)
+
+    # Destination (data/raw/)
+    dst = os.path.join("data", "raw", csv_name)
+    os.makedirs("data/raw", exist_ok=True)
+
+    # Copy only if not already there
+    if not os.path.exists(dst):
+        shutil.copy(src, dst)
+        print(f"Dataset copied to {dst}")
+    else:
+        print(f"Dataset already exists at {dst}")
+
+    return dst
+
+if __name__ == "__main__":
+    download_telco_churn()
